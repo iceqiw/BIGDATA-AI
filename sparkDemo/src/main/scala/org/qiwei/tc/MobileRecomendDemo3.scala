2f2b6d6023b4b0243894a64fda60dd8ef3be5640
iceqiw <qqwei1123@163.com>
2018-05-18 17:38:35 +0800

    pipeline

--- sparkDemo/src/main/scala/org/qiwei/tc/MobileRecomendDemo3.scala
+++ sparkDemo/src/main/scala/org/qiwei/tc/MobileRecomendDemo3.scala
@@ -1,6 +1,9 @@
 package org.qiwei.tc
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
 
 object MobileRecomendDemo3 {
@@ -15,7 +18,23 @@ object MobileRecomendDemo3 {
     import spark.implicits._
 
     val trainDF=spark.read.parquet("data/train/train_d_d.parquet")
-    trainDF.show()
+//    trainDF.filter($"label"===1).show()
+    val Array(trainingDF, testDF) = trainDF.randomSplit(Array(0.9, 0.1), seed = 12345)
+    val output_features= new VectorAssembler()
+      .setInputCols(Array("is_buy", "is_car", "is_favor", "is_browse", "label"))
+      .setOutputCol("features")
+//    trainingDF.show()
+//    testDF.show()
+
+    val lrModel = new LogisticRegression().setLabelCol("label").setFeaturesCol("features")
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+    val pipeline= new Pipeline().setStages(Array(output_features,lrModel))
+
+   val test1= pipeline.fit(trainingDF).transform(testDF)
+
+    test1.filter($"label"=!=$"prediction").show
     spark.stop()
   }
 }
